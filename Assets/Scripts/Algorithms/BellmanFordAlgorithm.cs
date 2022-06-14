100deacd327584c51135cdc34d73d5f47b7998c6
arinaldi <andrea.rinaldi54@gmail.com>
2022-06-14 08:54:26 +0200

    refactor: applied major refactor on BellmanFord algorithm

--- Assets/Scripts/Algorithms/BellmanFordAlgorithm.cs
+++ Assets/Scripts/Algorithms/BellmanFordAlgorithm.cs
@@ -5,11 +5,6 @@ namespace Assets.Scripts.Algorithms
 {
     class BellmanFordAlgorithm : Algorithm
     {
-        private static readonly float seconds = 0.00001f;
-        private readonly WaitForSeconds bellmanFordWfs = new WaitForSeconds(seconds);
-        private bool areAllEdgesRelaxed, negativeCycleExist = false;
-
-
         public override IEnumerator<WaitForSeconds> Run(Tile from, Tile to = null)
         {
             Dictionary<int, Tile> parent = new Dictionary<int, Tile>();
@@ -18,11 +13,9 @@ namespace Assets.Scripts.Algorithms
             dist[from.id] = 0;
             int v = 1;
 
-            // Relaxing all edges
-            while (v < matrix.Length && !areAllEdgesRelaxed)
+            // Relaxing all edges (V - 1) times
+            while (v < matrix.Length)
             {
-                areAllEdgesRelaxed = true;
-
                 for (int i = 0; i < matrix.GetLength(0); i++)
                 {
                     for (int j = 0; j < matrix.GetLength(1); j++)
@@ -30,106 +23,103 @@ namespace Assets.Scripts.Algorithms
                         Tile t = matrix[i, j];
 
                         if (!t.isObstacle)
-                        {
                             StartCoroutine(ParseAdjacentTiles(t, parent, dist));
-                        }
                     }
                 }
 
                 v++;
             }
 
-            // Checking for negative cycles
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    Tile t = matrix[i, j];
-
-                    if (!t.isObstacle)
-                        StartCoroutine(ParseAdjacentTiles(t, parent, dist, true));
-
-                    if (negativeCycleExist)
-                        break;
-                }
-
-                if (negativeCycleExist)
-                    break;
-            }
-
-            if (!negativeCycleExist)
-            {
-                StartCoroutine(HighlightShortestPath(parent, to));
-            }
-            else
+            if (CheckForNegativeCycles(parent, dist))
             {
                 print("Negative Cycle Exists");
             }
+            else
+            {
+                StartCoroutine(HighlightShortestPath(parent, to));
+            }
 
-            ResetAlgorithmAttributes();
+            GraphComponent.isAlgorithmRunning = false;
             yield return null;
         }
 
         /// <summary>
         /// Cycles over the 4 possibile grid directions
         /// </summary>
-        /// <param name="from">The starting tile(vertex)</param>
+        /// <param name="from">The tile that is currently being parsed(vertex)</param>
         /// <param name="parent">The dictionary holding the parent reference for every tile, useful to reconstruct the shortest path</param>
         /// <param name="dist">The array holding the shortest distance for every node in the graph</param>
         /// <returns>The current instance 'wfs' coroutine attribute when a tile is added to the queue</returns>
-        private IEnumerator<WaitForSeconds> ParseAdjacentTiles(
-            Tile from,
-            Dictionary<int, Tile> parent,
-            int[] dist,
-            bool checkForNegativeCycles = false
-        )
+        private IEnumerator<WaitForSeconds> ParseAdjacentTiles(Tile from, Dictionary<int, Tile> parent, int[] dist)
         {
             for (byte i = 0; i < Constant.DirectionsNumber; i++)
             {
                 Tile tile = RetrieveAdjacentTile(from.x + rd[i], from.y + cd[i]);
-
                 if (tile == null)
                     continue;
 
-                // Computing new distance/weight and checking for integer overflows
-                int newDist;
-
-                try
-                {
-                    newDist = checked(dist[from.id] + tile.Weight);
-                }
-                catch (System.OverflowException)
-                {
-                    newDist = int.MaxValue;
-                }
+                int newDist = CalculateNewDistance(dist[from.id], tile.Weight);
 
                 if (newDist < dist[tile.id])
                 {
-                    if (!checkForNegativeCycles)
-                    {
-                        areAllEdgesRelaxed = false;
-                        dist[tile.id] = newDist;
-                        parent[tile.id] = from;
+                    dist[tile.id] = newDist;
+                    parent[tile.id] = from;
 
-                        yield return bellmanFordWfs;
-                    }
-                    else
-                    {
-                        negativeCycleExist = true;
-                        break;
-                    }
+                    yield return null;
                 }
             }
         }
 
         /// <summary>
-        /// Resets instante Algorithm attributes to default values
+        /// Executes the Bellman ford algorithm one time in order to detect the existance of negative cycles
         /// </summary>
-        private void ResetAlgorithmAttributes()
+        /// <param name="parent">The dictionary holding the parent reference for every tile, useful to reconstruct the shortest path</param>
+        /// <param name="dist">The array holding the shortest distance for every node in the graph</param>
+        /// <returns>Whether the current graph contains negative cycles or not</returns>
+        private bool CheckForNegativeCycles(Dictionary<int, Tile> parent, int[] dist)
         {
-            GraphComponent.isAlgorithmRunning = false;
-            areAllEdgesRelaxed = false;
-            negativeCycleExist = false;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Tile t = matrix[i, j];
+
+                    if (t.isObstacle)
+                        continue;
+
+                    for (byte z = 0; z < Constant.DirectionsNumber; z++)
+                    {
+                        Tile tile = RetrieveAdjacentTile(t.x + rd[z], t.y + cd[z]);
+                        if (tile == null)
+                            continue;
+
+                        if (CalculateNewDistance(dist[t.id], tile.Weight) < dist[tile.id])
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Calculates the distance from one tile to another checking for INT overflows
+        /// </summary>
+        /// <param name="currentTileDist">The distance/weight that has been previously calculated for the current tile</param>
+        /// <param name="nextTileWeight">The cost needed to arrive to the given tile</param>
+        /// <returns>The new distance value</returns>
+        private int CalculateNewDistance(int currentTileDist, int nextTileWeight)
+        {
+            try
+            {
+                return checked(currentTileDist + nextTileWeight);
+            }
+            catch (System.OverflowException)
+            {
+                return int.MaxValue;
+            }
         }
     }
 }
